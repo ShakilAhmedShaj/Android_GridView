45bb10ad73bb352e63dce47c06ce97fb65ff5bff
SHAJ <shakilahmedshaj@gmail.com>
2018-09-20 23:15:01 +0600

    listener added

--- app/src/main/java/com/t3ch/shaj/android_gridview/MainActivity.java
+++ app/src/main/java/com/t3ch/shaj/android_gridview/MainActivity.java
@@ -2,13 +2,16 @@ package com.t3ch.shaj.android_gridview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private GridView gridView;
 
-    int[] animals = {R.drawable.cat,R.drawable.dog,R.drawable.monkey,R.drawable.tiger,R.drawable.horse,R.drawable.elephant,R.drawable.lion,R.drawable.bear,R.drawable.rabbit};
+    int[] animals = {R.drawable.cat, R.drawable.dog, R.drawable.monkey, R.drawable.tiger, R.drawable.horse, R.drawable.elephant, R.drawable.lion, R.drawable.bear, R.drawable.rabbit};
 
     String[] animalNames;
 
@@ -21,13 +24,21 @@ public class MainActivity extends AppCompatActivity {
 
         animalNames = getResources().getStringArray(R.array.animals);
 
-        CustomAdapter adapter = new CustomAdapter(this,animalNames,animals);
+        CustomAdapter adapter = new CustomAdapter(this, animalNames, animals);
 
         gridView.setAdapter(adapter);
 
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String value = animalNames[position];
+
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
 
 
-
+            }
+        });
 
 
     }
